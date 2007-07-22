a2fd573ef168e736b2046173c993e9650fb8568d
Jakub Wilk <jwilk@jwilk.net>
2007-07-22 11:18:09 +0000

    + setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'dime',
+	version = '0.1',
+	url = 'http://code.google.com/p/python-dime/',
+	author = 'Jakub Wilk',
+	author_email = 'ubanus@users.sf.net',
+	py_modules = ['dime']
+)
+
+# vim:ts=4 sw=4 noet
