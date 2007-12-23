34e7e05ac7abd5bdf49db0c8fd30cef0be469a42
Jakub Wilk <jwilk@jwilk.net>
2007-12-23 15:25:47 +0000

    setup.py: more metadata.

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
+'''
+python-dime provides a way to parse and generate DIME messages.
+
+Direct Internet Message Encapsulation (DIME) is a binary message format that can be used to encapsulate multiple payloads into a single message.\
+'''
+
+classifiers = '''\
+Development Status :: 3 - Alpha
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules'''.split('\n')
+
 from distutils.core import setup
 
 setup(
 	name = 'dime',
 	version = '0.1',
+	license = 'GNU GPL 2',
+	platforms = ['any'],
+	description = 'DIME message processing',
+	long_description = __doc__.strip(),
+	classifiers = classifiers,
 	url = 'http://code.google.com/p/python-dime/',
 	author = 'Jakub Wilk',
 	author_email = 'ubanus@users.sf.net',
